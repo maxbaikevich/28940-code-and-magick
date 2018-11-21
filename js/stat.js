e04d84d1a38819c895420dd4b11f045331bdc04f
Maxim Baykevich <max.baikevich@gmail.com>
2018-11-21 22:50:27 +0300

    Отрисовал облоко и тень

--- js/stat.js
+++ js/stat.js
@@ -8,8 +8,21 @@
   Цвет колонки игрока Выrgba(255, 0, 0, 1).
   Цвет колонок других игроков — синий, а насыщенность задаётся случайным образом.
 */
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 
-window.renderStatistics = function() {
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+window.renderStatistics = function(ctx) {
+  renderCloud(ctx, 110, 20, 420, 270 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, 420, 270 '#fff');
+
   ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 10, 270, 420);
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вы', 110, 75);
+  ctx.fillRect(110, 60, 480, 20); 
 };
